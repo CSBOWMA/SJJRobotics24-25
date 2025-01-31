5663458ca467cf3af1a9c6c9c8d2e9b351a1f36c
caden <cadenbowman53@gmail.com>
2025-01-30 23:24:34 -0500

    Wrote out functionality of teleop RobotFullAbsolute

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFullAbsolute.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFullAbsolute.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode;
+import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -52,9 +53,85 @@ public class RobotFullAbsolute extends LinearOpMode {
 
         backLeftDrive = hardwareMap.get(DcMotor.class, "backleft");
         backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+
+
         odom = hardwareMap.get(DcMotor.class, "straight");
         odom.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         odom.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+        Servo outtakeAngle;
+        final double OUTTAKE_ANGLE_DROP_POSITION = .589+.03;
+        final double OUTTAKE_ANGLE_LOAD_POSITION = .441+0.028;
+        final double OUTTAKE_ANGLE_READY_LOAD_POSITION = .443+0.028;
+
+        Servo outtakeClaw;
+        final double OUTTAKE_CLAW_OPEN_POSITION = 0.2;
+        final double OUTTAKE_CLAW_CLOSED_POSITION = 0.34;
+
+
+        Servo intakeAngle;
+        final double INTAKE_ANGLE_SEARCH_POSITION;
+        final double INTAKE_ANGLE_LOAD_POSITION = .75;
+        final double INTAKE_ANGLE_GRAB_POSITION = .06;
+
+        Servo intakeClaw;
+        final double INTAKE_CLAW_OPEN_POSITION = .0;
+        final double INTAKE_CLAW_CLOSED_POSITION = 0.052;
+
+        Servo intakePivot;
+        final double INTAKE_PIVOT_HIGH_TURN_POSITION;
+        final double INTAKE_PIVOT_LOW_TURN_POSITION;
+        final double INTAKE_PIVOT_PASS_POSITION;
+        boolean positiveRotate = true;
+
+        //private Servo intakeSlide1; needs to be implemented
+        //private Servo intakeSlide2; needs to be implemented
+
+        outtakeAngle = hardwareMap.get(Servo.class, "outtakeAngle");
+        outtakeClaw = hardwareMap.get(Servo.class, "outtakeClaw");
+        outtakeAngle.setPosition(OUTTAKE_ANGLE_LOAD_POSITION);
+        outtakeClaw.setPosition(OUTTAKE_CLAW_CLOSED_POSITION);
+        intakeAngle = hardwareMap.get(Servo.class, "intakeAngle");
+        intakeAngle.setPosition(INTAKE_ANGLE_LOAD_POSITION);
+        intakeClaw = hardwareMap.get(Servo.class, "intakeClaw");
+
+        intakeClaw.setPosition(INTAKE_CLAW_OPEN_POSITION);
+
+
+
+        DcMotor elevator1;
+        DcMotor elevator2;
+
+
+        elevator1 = hardwareMap.get(DcMotor.class, "elavator1");
+        elevator1.setDirection(DcMotorSimple.Direction.REVERSE);
+        elevator1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        elevator2 = hardwareMap.get(DcMotor.class, "elavator2");
+        elevator2.setDirection(DcMotorSimple.Direction.FORWARD);
+        elevator2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        elevator1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        elevator2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        final int LOW_ELEVATOR_POSITION = 0;
+        final int HIGH_ELEVATOR_POSITION = 3300;
+
+        RevColorSensorV3 frontSensor;
+        RevColorSensorV3 backSensor;
+        final int COLORTHRESHOLD = 128;
+        boolean XPressed = false;
+
+        int currentMode = 0;
+        final int DEFAULTMODE = 0;
+        final int SEARCHMODE = 1;
+        final int GRABMODE = 2;
+        final int PASSMODE = 3;
+        final int ELEVATORMODE = 4;
+        final int READYDROPMODE = 5;
+        final int DROPMODE = 6;
+
+
 
 
         //   frontRightDrive = hardwareMap.get(DcMotor.class, "frontRight");
@@ -74,10 +151,11 @@ public class RobotFullAbsolute extends LinearOpMode {
         waitForStart();
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
+            //all drive control, joysticks
             currentFacing = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
 
             toGo.setVector(-gamepad1.left_stick_y, -gamepad1.left_stick_x);
-            // toGo.normalizeVector();
+
             toGo.setRelative(currentFacing);
 
             speed = toGo.getI() * TOTALSPEED;
@@ -113,12 +191,157 @@ public class RobotFullAbsolute extends LinearOpMode {
 
             turn *= direction.getMagnitude();
 
+
+            telemetry.addData("ticks", odom.getCurrentPosition());
+
+            if (gamepad1.a) {
+                currentMode = SEARCHMODE;
+            }
+            switch (currentMode) {
+                case SEARCHMODE:
+
+                    if (intakePivot.getPosition() > INTAKE_PIVOT_HIGH_TURN_POSITION - .02) {
+                        positiveRotate = false;
+                    }
+
+                    if (intakePivot.getPosition() < INTAKE_PIVOT_LOW_TURN_POSITION + .02) {
+                        positiveRotate = true;
+                    }
+
+                    if (frontSensor.red() > COLORTHRESHOLD && backSensor.red() > COLORTHRESHOLD
+                            || frontSensor.green() > COLORTHRESHOLD && backSensor.green() > COLORTHRESHOLD) {
+                        intakePivot.setPosition(intakePivot.getPosition());
+                        intakeAngle.setPosition(INTAKE_ANGLE_GRAB_POSITION);
+                        if (!gamepad1.a) {
+                            currentMode = GRABMODE;
+                        }
+                    }
+
+                    else if (positiveRotate) {
+                        intakePivot.setPosition(INTAKE_PIVOT_HIGH_TURN_POSITION);
+
+                    } else {
+                        intakePivot.setPosition(INTAKE_PIVOT_LOW_TURN_POSITION);
+                    }
+
+                    elevator1.setTargetPosition(LOW_ELEVATOR_POSITION);
+                    elevator2.setTargetPosition(LOW_ELEVATOR_POSITION);
+                    elevator1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    elevator2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    outtakeAngle.setPosition(OUTTAKE_ANGLE_LOAD_POSITION);
+                    outtakeClaw.setPosition(OUTTAKE_CLAW_OPEN_POSITION);
+                    intakeClaw.setPosition(INTAKE_CLAW_OPEN_POSITION);
+                    intakeAngle.setPosition(INTAKE_ANGLE_SEARCH_POSITION);
+
+                    break;
+                case GRABMODE:
+                    speed = 0;
+                    strafe = 0;
+                    turn = 0;
+                    intakeAngle.setPosition(INTAKE_ANGLE_GRAB_POSITION);
+
+                    if(intakeAngle.getPosition() > INTAKE_ANGLE_GRAB_POSITION - .02
+                            && intakeAngle.getPosition() < INTAKE_ANGLE_GRAB_POSITION + .02) {
+                        intakeClaw.setPosition(INTAKE_CLAW_CLOSED_POSITION);
+                    }
+
+                    if (intakeClaw.getPosition() > INTAKE_CLAW_CLOSED_POSITION -.02
+                    && intakeClaw.getPosition() < INTAKE_CLAW_CLOSED_POSITION + .02 && frontSensor.red() > COLORTHRESHOLD && backSensor.red() > COLORTHRESHOLD
+                            || frontSensor.green() > COLORTHRESHOLD && backSensor.green() > COLORTHRESHOLD) {
+                        currentMode = PASSMODE;
+                    } else if (intakeClaw.getPosition() > INTAKE_CLAW_CLOSED_POSITION -.02
+                    && intakeClaw.getPosition() < INTAKE_CLAW_CLOSED_POSITION + .02) {
+                        currentMode = SEARCHMODE;
+                    }
+
+                    elevator1.setTargetPosition(LOW_ELEVATOR_POSITION);
+                    elevator2.setTargetPosition(LOW_ELEVATOR_POSITION);
+                    elevator1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    elevator2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    outtakeAngle.setPosition(OUTTAKE_ANGLE_LOAD_POSITION);
+                    outtakeClaw.setPosition(OUTTAKE_CLAW_OPEN_POSITION);
+                    break;
+                case PASSMODE:
+
+                    intakeAngle.setPosition(INTAKE_ANGLE_LOAD_POSITION);
+                   if(!(intakeAngle.getPosition() < INTAKE_ANGLE_LOAD_POSITION + .02
+                    && intakeAngle.getPosition() > INTAKE_ANGLE_LOAD_POSITION - .02)) {
+                       outtakeAngle.setPosition(OUTTAKE_ANGLE_LOAD_POSITION);
+                       outtakeClaw.setPosition(OUTTAKE_CLAW_OPEN_POSITION);
+                       elevator1.setTargetPosition(LOW_ELEVATOR_POSITION);
+                       elevator2.setTargetPosition(LOW_ELEVATOR_POSITION);
+                       elevator1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                       elevator2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                   } else if (outtakeClaw.getPosition() < OUTTAKE_CLAW_CLOSED_POSITION + .02
+                   && outtakeClaw.getPosition() > OUTTAKE_CLAW_CLOSED_POSITION - .02){
+                       outtakeClaw.setPosition(OUTTAKE_CLAW_OPEN_POSITION);
+                   } else if (intakeClaw.getPosition() < INTAKE_CLAW_OPEN_POSITION + .02
+                   && intakeClaw.getPosition() > INTAKE_CLAW_OPEN_POSITION - .02){
+                       currentMode = ELEVATORMODE;
+                   }
+
+                    break;
+                case ELEVATORMODE:
+                    intakeAngle.setPosition(INTAKE_ANGLE_LOAD_POSITION);
+                    elevator1.setTargetPosition(HIGH_ELEVATOR_POSITION);
+                    elevator2.setTargetPosition(HIGH_ELEVATOR_POSITION);
+                    elevator1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    elevator2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    outtakeAngle.setPosition(OUTTAKE_ANGLE_DROP_POSITION);
+                    outtakeClaw.setPosition(OUTTAKE_CLAW_CLOSED_POSITION);
+
+                    if (elevator1.getCurrentPosition() > HIGH_ELEVATOR_POSITION-50
+                    && elevator2.getCurrentPosition() > HIGH_ELEVATOR_POSITION-50) {
+                        currentMode = READYDROPMODE;
+                    }
+
+                    break;
+                case READYDROPMODE:
+
+                    if (gamepad1.b) {
+                        currentMode = DROPMODE;
+                    } else {
+
+                        intakeAngle.setPosition(INTAKE_ANGLE_LOAD_POSITION);
+                        elevator1.setTargetPosition(HIGH_ELEVATOR_POSITION);
+                        elevator2.setTargetPosition(HIGH_ELEVATOR_POSITION);
+                        elevator1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        elevator2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        outtakeAngle.setPosition(OUTTAKE_ANGLE_DROP_POSITION);
+                        outtakeClaw.setPosition(OUTTAKE_CLAW_CLOSED_POSITION);
+                    }
+                    break;
+
+                case DROPMODE:
+                    outtakeClaw.setPosition(OUTTAKE_CLAW_OPEN_POSITION);
+                    speed = 0;
+                    strafe = 0;
+                    turn = 0;
+                    if (outtakeClaw.getPosition() < OUTTAKE_CLAW_CLOSED_POSITION + .02
+                    && outtakeClaw.getPosition() > OUTTAKE_ANGLE_READY_LOAD_POSITION - .02) {
+                        currentMode = DEFAULTMODE;
+                    }
+                default:
+
+                    intakePivot.setPosition(INTAKE_PIVOT_PASS_POSITION);
+                    intakeClaw.setPosition(INTAKE_CLAW_OPEN_POSITION);
+                    intakeAngle.setPosition(INTAKE_ANGLE_LOAD_POSITION);
+                    outtakeAngle.setPosition(OUTTAKE_ANGLE_LOAD_POSITION);
+                    outtakeClaw.setPosition(OUTTAKE_CLAW_OPEN_POSITION);
+                    elevator1.setTargetPosition(LOW_ELEVATOR_POSITION);
+                    elevator2.setTargetPosition(LOW_ELEVATOR_POSITION);
+                    elevator1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    elevator2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            }
+
+
             frontLeftDrive.setPower(speed + strafe + turn);
             frontRightDrive.setPower(speed - strafe - turn);
             backLeftDrive.setPower(speed - strafe + turn);
             backRightDrive.setPower(speed + strafe - turn);
+            //end drive
 
-            telemetry.addData("ticks", odom.getCurrentPosition());
             //the rest of the code goes here
 
         }
